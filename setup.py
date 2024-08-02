f2f5142b669f8fcbba6f6f616fbe693886d3c9e2
iqbal <iqbal@bytesview.com>
2024-08-02 11:44:23 +0530

    added github action workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
-        'firebase-admin',
+        'firebase-admin','rq','redis',
     ],
     author='BytesView',
     author_email='contact@bytesview.com',
