5fafed76c6541fa6a0e561575cf9bf830f57bb13
chirag <chirag@bytesview.com>
2024-10-17 14:36:56 +0530

    onesignal integrated

--- BytesViewFCM/notification_exception.py
+++ BytesViewFCM/notification_exception.py
@@ -4,3 +4,11 @@ class NotificationException(Exception):
 
     def __init__(self, Error):
         self.Error = Error
+
+class TableNotExistError(NotificationException):
+    def __init__(self, error):
+        super().__init__(error)
+
+class RateLimitExceeded(NotificationException):
+    def __init__(self, error):
+        super().__init__(error)
